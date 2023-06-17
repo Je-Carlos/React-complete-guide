d3ecd13fd580975396128b24bf03107d8ea77137
JeanCarlos <cpjeanberg@gmail.com>
2023-06-17 16:12:54 -0300

    adicionando props ao expenseItem

--- react-basics-and-components/src/components/ExpenseItem.js
+++ react-basics-and-components/src/components/ExpenseItem.js
@@ -1,13 +1,30 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  // formatando data para dd/mm/yyyy
+  function FormatedExpenseDate(oldDate) {
+    const date = new Date(oldDate);
+    const day = date.getDate();
+    const month = date.getMonth() - 1;
+    const year = date.getFullYear();
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const newDate = new Date(year, month, day);
+    return newDate.toLocaleDateString("pt-BR", options);
+  }
+
   return (
     <div className="expense-item">
-      <div>17 de Junho 2023</div>
-      <div className="expense-item__description">
-        <h2>Teclado</h2>
+      <div className="expense-item__date">
+        {FormatedExpenseDate(props.date)}
       </div>
-      <div className="expense-item__price">R$ 144.90</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">R$ {props.amount.toFixed(2)}</div>
     </div>
   );
 }
