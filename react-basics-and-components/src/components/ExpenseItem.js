a95b594af5cded1ac567df498edb2c02c43cfa9f
JeanCarlos <cpjeanberg@gmail.com>
2023-06-18 23:23:11 -0300

    separando ExpanseDate e Expenses, + estilizar

--- react-basics-and-components/src/components/ExpenseItem.js
+++ react-basics-and-components/src/components/ExpenseItem.js
@@ -1,26 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  // formatando data para dd/mm/yyyy
-  function FormatedExpenseDate(oldDate) {
-    const date = new Date(oldDate);
-    const day = date.getDate();
-    const month = date.getMonth() - 1;
-    const year = date.getFullYear();
-    const options = {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
-    const newDate = new Date(year, month, day);
-    return newDate.toLocaleDateString("pt-BR", options);
-  }
-
   return (
     <div className="expense-item">
-      <div className="expense-item__date">
-        {FormatedExpenseDate(props.date)}
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
