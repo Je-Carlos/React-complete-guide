7b33ebac9b3aad065612e535eea9e4e0aeb05270
JeanCarlos <cpjeanberg@gmail.com>
2023-06-20 19:56:27 -0300

    outra forma de lidar com event Handler com useState

--- 3-react-state-and-events/src/components/NewExpense/ExpenseForm.jsx
+++ 3-react-state-and-events/src/components/NewExpense/ExpenseForm.jsx
@@ -2,18 +2,32 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredNumber, setEnteredNumber] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredNumber, setEnteredNumber] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredNumber: "",
+    enteredDate: "",
+  });
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   };
   const amountChangeHandler = (event) => {
-    setEnteredNumber(event.target.value);
+    // setEnteredNumber(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredNumber: event.target.value,
+    });
   };
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+    setUserInput({ ...userInput, enteredDate: event.target.value });
   };
 
   return (
