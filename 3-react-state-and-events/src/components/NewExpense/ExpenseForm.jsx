79aa12f7597e68496a8e621cf06a938fa2986007
JeanCarlos <cpjeanberg@gmail.com>
2023-06-20 20:07:46 -0300

    forma segura de retornar um prevState sem  a chance de perder o valor correto

--- 3-react-state-and-events/src/components/NewExpense/ExpenseForm.jsx
+++ 3-react-state-and-events/src/components/NewExpense/ExpenseForm.jsx
@@ -2,32 +2,44 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredNumber, setEnteredNumber] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredNumber: "",
-    enteredDate: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredNumber, setEnteredNumber] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredNumber: "",
+  //   enteredDate: "",
+  // });
 
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
-    });
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
   };
   const amountChangeHandler = (event) => {
-    // setEnteredNumber(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredNumber: event.target.value,
-    });
+    setEnteredNumber(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredNumber: event.target.value,
+    // });
   };
   const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
+    setEnteredDate(event.target.value);
+    /* --------------------------------------------------------------------------------
+    Essa forma pode dar problema devido a natureza assíncrona do React, ele pode 
+    não atualizar o valor correto 
+
     setUserInput({ ...userInput, enteredDate: event.target.value });
+    --------------------------------------------------------------------------------
+    Essa forma é a mais segura, pois o React garante que o valor será atualizado 
+
+    setUserInput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value };
+    });
+    --------------------------------------------------------------------------------
+    */
   };
 
   return (
