4c0992a774debca5f26e17b4a02034de260908cc
Je-Carlos <cpjeanberg@gmail.com>
2023-06-22 10:52:50 -0300

    como passar componentes de filhos para pais

--- 3-react-state-and-events/src/App.js
+++ 3-react-state-and-events/src/App.js
@@ -27,10 +27,13 @@ const App = () => {
   //   React.createElement("h2", {}, "Let's get started!"),
   //   React.createElement(Expenses, { items: expenses })
   // );
-
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
