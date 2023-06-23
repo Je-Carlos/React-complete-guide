72da589885b9e8017e6729edf35527f99a80466c
Je-Carlos <cpjeanberg@gmail.com>
2023-06-22 22:57:40 -0300

    ajustando código, e comentando exemplos

--- 3-react-state-and-events/src/components/Expenses/ExpenseItem.js
+++ 3-react-state-and-events/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
   // function clickHandler() {}
-  const clickHandler = () => {
-    setTitle("Atualizado!");
-  };
+  // const clickHandler = () => {
+  //   setTitle("Atualizado!");
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">R$ {props.amount.toFixed(2)}</div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 };
